2df16856f3827179ba06a14b1c55c563798a3869
Glen H.M. Sørensen <gsorensen1201@gmail.com>
2024-01-11 14:58:53 +0100

    Create wong_colour_palette.m

---
+++ wong_colour_palette.m
@@ -0,0 +1,14 @@
+%{ Matlab vector representing the colour palette described in this paper by Wong: https://www.nature.com/articles/nmeth.1618
+Requires the following add-on https://se.mathworks.com/matlabcentral/fileexchange/46289-rgb2hex-and-hex2rgb?s_tid=ta_fx_results 
+%} Order can be changed based on personal preference, name of colours in comments based off of the following article: https://cambiocteach.com/accessibility/colourchoice/
+
+wong_colour_palette = [
+    hex2rgb("0072b2"); % blue
+    hex2rgb("e69f00"); % orange
+    hex2rgb("009e73"); % blue green
+    hex2rgb("cc79a7"); % pale violet 
+    hex2rgb("56b4e9"); % sky blue
+    hex2rgb("d55e00"); % vermillion
+    hex2rgb("f0e442"); % yellow
+    hex2rgb("000000"); % black
+];
